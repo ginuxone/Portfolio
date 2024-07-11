4f008c7b9c0c9a3359723a1b52d033799e69044e
gino alessandro milla <ginomilla@outlook.com>
2024-07-11 19:43:36 +0200

    Moved Homepage
    Moved homepage to match a more structured criteria

---
+++ lib/Pages/homepage.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+class Homepage extends StatefulWidget {
+  const Homepage({Key? key}) : super(key: key);
+
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Text(AppLocalizations.of(context)!.homepageTitle),
+          ],
+        ),
+      )
+    );
+  }
+}
